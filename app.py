d0b786be3bff786129c585fc518ad2ec92a5d287
Verma <212677783@logon.ds.ge.com>
2020-01-27 18:30:53 +0800

    Add functionality to reset and display results in frontend

--- app.py
+++ app.py
@@ -24,20 +24,27 @@ def predict():
     if GB:
         try:
             int_features = [str(x) for x in request.form.values()]
-            #final_features = [np.array(int_features)]
             top_features_include = ['odor', 'bruises', 'gill-spacing', 'gill-size', 'ring-type']
 #            json_ = request.form.values()
             dict_use = dict(zip(top_features_include, int_features))
-            print('Predicting for paramters',int_features,top_features_include)
+#            print('Predicting for paramters',int_features,top_features_include)
             
             query = pd.get_dummies(pd.DataFrame(dict_use,index=[0]))
             query = query.reindex(columns=model_columns, fill_value=0)
 
-            prediction = list(GB.predict(query))
-            print ('Result: ',prediction)
+            prediction = GB.predict(query)
+            if prediction == 0:
+                class_mushroom = "Edible"
+            else:
+                class_mushroom = "Poisonous"
+                
+#            print ('Result: ',class_mushroom)
+            output = 'Your Mushroom is : '+ class_mushroom
+#            print(output)
 
-            return jsonify({'prediction': str(prediction)})
-            return render_template('main.html', features='Mushroom is'.format(prediction))
+#            return jsonify({'prediction': str(prediction)})
+#            return render_template('main.html', features='Your Mushroom is'.format(class_mushroom))
+            return render_template('main.html', features=output)
 
         except:
 
@@ -64,6 +71,6 @@ GB = joblib.load("model/mushroom_model.pkl") # Load "model.pkl"
 print ('Model loaded')
 model_columns = joblib.load("model/model_columns.pkl") # Load "model_columns.pkl"
 print ('Model columns loaded')
-print(model_columns)
+#print(model_columns)
 
 app.run(port=port, debug=True)
